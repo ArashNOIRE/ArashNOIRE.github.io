2b84dba8041bd1272fc48b3425e34185dc5daef4
A$HY <Arash.noire@outlook.com>
2025-03-14 14:30:12 +0330

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,12 @@
+const User = "Hello, world!";
+
+console.log(User);
+function Alert () {
+    alert("کاربر عزیز سازنده این سایت تازه کار است و هنوز این قسمت تکمیل نشده است!!!");
+}
+// ==> Prank!!!
+function POP (){
+    for (let i = 2; i > 1; i++) {
+        window.open('https://www.google.com/', '_blank');
+    }
+}
