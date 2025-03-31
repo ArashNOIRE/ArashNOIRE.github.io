aada8c6e7473a69c8587e5b33287b9795e5f11a1
ASH <Arash.noire@outlook.com>
2025-03-31 12:34:58 +0330

    Add files via upload
    Now I can center a div
    JK

--- Main.js
+++ Main.js
@@ -1,11 +1,9 @@
 const User = "Hello, world!";
-console.log(User);
 
-// ==> massage
+console.log(User);
 function Alert () {
     alert("کاربر عزیز سازنده این سایت تازه کار است و هنوز این قسمت تکمیل نشده است!!!");
 }
-
 // ==> Prank!!!
 function POP (){
     for (let i = 2; i > 1; i++) {
