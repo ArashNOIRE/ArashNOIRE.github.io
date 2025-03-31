587d93bf1eb88dd5cbe0e01e76c6ccb5076344ae
ASH <Arash.noire@outlook.com>
2025-03-31 17:25:59 +0330

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,10 @@ function POP (){
     for (let i = 2; i > 1; i++) {
         window.open('https://www.google.com/', '_blank');
     }
+} 
+function git_Game () {
+    window.open('https://github.com/ArashNOIRE/Game_Collection', '_blank');
+}
+function git_site () {
+    window.open('https://github.com/ArashNOIRE/ArashNOIRE.github.io', '_blank');
 }
